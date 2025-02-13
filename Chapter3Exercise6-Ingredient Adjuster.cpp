52d4d5a5e6e4d5452e4eceab266fafc2bd3caf8c
Morgan Thorne <mt34452@email.vccs.edu>
2025-02-13 12:41:31 -0500

    Add project files.

---
+++ Chapter3Exercise6-Ingredient Adjuster.cpp
@@ -0,0 +1,30 @@
+/*
+FileName: Chapter3Exercise6-IngredientAdjuster.cpp
+Programmer: Morgan Thorne
+Date: February 2025
+Requirements:
+Write a program that asks the user how many cookies they want to make,
+then display the number of cups pf each ingredient needed for the specified number of cookies
+*/
+
+#include <iostream>
+#include <iomanip>
+using names
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
